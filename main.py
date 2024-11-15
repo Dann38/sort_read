beb820b574b7b9145d7f8f17cda1ec9ebc8f1456
Mike <karpenkov.2004@mail.ru>
2024-11-15 21:14:50 +0800

    Реализовал сравнение двух блоков. Поправил

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from abc import ABC
+from typing import Dict
 
 
 class BBox(ABC):
 
-    def __init__(self, cord):
+    def __init__(self, cord : Dict[str, int]):
         self.x0 = cord["x0"]
         self.x1 = cord["x1"]
         self.y0 = cord["y0"]
         self.y1 = cord["y1"]
 
-    def __gt__(self, bbox):
+    def __gt__(self, bbox : "BBox"):
         eps = max(self.y0 - self.y1, bbox.y0 - bbox.y1)
         if self.y0 > bbox.y0 + eps:
             return True
