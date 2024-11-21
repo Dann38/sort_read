21a03f203d3c13f7a98a52109293d330d124d443
Mike <karpenkov.2004@mail.ru>
2024-11-21 11:27:12 +0800

    Убрал лишние тесты в main

--- main.py
+++ main.py
@@ -17,13 +17,3 @@ class BBox(ABC):
         elif abs(bbox.y0 - self.y0) < eps and bbox.x0 > self.x0:
             return True
         return False
-
-first_bbox = BBox({"x0":10, "y0" : 10 , "x1" : 40, "y1" : 20})
-second_bbox = BBox({"x0":50, "y0" : 10 , "x1" : 110, "y1" : 20})
-third_bbox = BBox({"x0":10, "y0" : 30 , "x1" : 60, "y1" : 40})
-fourth_bbox = BBox({"x0":70, "y0": 30, "x1" : 110, "y1" : 40})
-print("Первый дальше третьего", first_bbox > third_bbox)
-print("Второй дальше первого", second_bbox > first_bbox)
-print("Четвертый дальше третьего", fourth_bbox > third_bbox)
-print("Четвертый дальше первого", fourth_bbox > first_bbox)
-print("Второй дальше четвертого", second_bbox > fourth_bbox)
