49ee6862166f55c78740fd417dbb3cebcf50e9f6
Mihail Karpenkov <karpenkov211004@mail.ru>
2024-12-18 19:17:21 +0800

    Изменил open(), добавил staticmethod get_points

--- tests/test_newBbox_reading_order.py
+++ tests/test_newBbox_reading_order.py
@@ -7,9 +7,23 @@ from SortableBBox import SortableBBox
 class test_reading_order_sort(unittest.TestCase):
     
     
+    @staticmethod
+    def get_points(line):
+        points = []
+        xs = []
+        ys = []
+        for point in range(len(line)):
+            if point%2==0:
+                xs.append(line[point])
+            else:
+                ys.append(line[point])
+        for i in range(len(xs)):
+            points.append((xs[i], ys[i]))
+        return points
+            
     def test_order(self):
         filenames = os.listdir('testing_imgs')
-        images = os.listdir('tests/results')
+        images = os.listdir(os.path.join('tests', 'results'))
         
         image_reader = ImageReader()
         tesseract_config = TesseractReaderConfig()
@@ -17,11 +31,11 @@ class test_reading_order_sort(unittest.TestCase):
         path_project = os.path.abspath(os.path.join(os.getcwd(), "."))
 
         for i in range(len(filenames)):
-            file = open('tests/results/' + images[i], 'r')
-            line = file.readline().split()
-            line = [float(line[i]) for i in range(len(line))]
-            
-            
+            with open(os.path.join(os.path.join('tests', 'results'), images[i]), 'r') as file:
+                line = file.readline().split()
+                line = [float(line[i]) for i in range(len(line))]
+                
+            points = test_reading_order_sort.get_points(line)
             path_img = os.path.join(path_project, 'testing_imgs', filenames[i])
             img = image_reader.read(path_img)
             readed_img = tesseract_reader.read(img)
@@ -32,5 +46,4 @@ class test_reading_order_sort(unittest.TestCase):
             current_result = Bbox_sorter.sort_bboxes(readed_img=readed_img)
             
             for index, result in zip([_ for _ in range(len(current_result))], current_result):
-                self.assertTrue(readed_img[0][result].point_is_in_bbox((line[2*index], line[2*index+1])))
-        
+                self.assertTrue(readed_img[0][result].point_is_in_bbox(points[index]))
